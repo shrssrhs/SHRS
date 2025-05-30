fafa0e0120bfdbe1b66b364935f5aeefad8d9fe2
shrssrhs <shenshenus42@gmail.com>
2025-05-30 20:53:43 +0200

    Add files via upload

---
+++ shrs-site/app/layout.tsx
@@ -0,0 +1,18 @@
+// app/layout.tsx
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'SHRS',
+  description: 'Next-gen communication by SHRS',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
